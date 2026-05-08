f53ab7f25841132ef86d2e45b44a6aebdcdac6fe
Dane Anderson <dane.anderson@gmail.com>
2026-05-08 14:24:28 -0500

    Support database URL from environment

--- database.py
+++ database.py
@@ -1,11 +1,14 @@
+import os
 import streamlit as st
 from sqlalchemy import create_engine
 
 
 @st.cache_resource
 def get_engine():
+    database_url = os.getenv("DATABASE_URL")
 
-    database_url = st.secrets["DATABASE_URL"]
+    if not database_url:
+        database_url = st.secrets["DATABASE_URL"]
 
     engine = create_engine(database_url)
 
